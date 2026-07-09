431b61e5c05a6738514215984d2fb9428edd4b75
Boaz Minitzer <36307182+bminitzer@users.noreply.github.com>
2026-07-09 14:00:13 -0700

    Delete Routes.tf

--- Routes.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ec2_transit_gateway_route" "west1_to_west2_vpc_172_17" {
-  provider = aws.west1
-
-  destination_cidr_block         = "172.17.0.0/16"
-  transit_gateway_route_table_id = "tgw-rtb-00538883cba480478"
-  transit_gateway_attachment_id  = "tgw-attach-0c7ecc882da2c7801"
-}
